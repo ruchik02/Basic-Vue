256de17a77ceae5386349ef2e3aabb97f5bc1a37
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-02-24 14:27:31 +0530

    basic increment and decrement button

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+export default {
+  data(){
+    return{
+      name: 'Ruchika Sharma',
+      count: 0
+    }
+  },
+  methods: {
+    increment(){
+      this.count++;
+
+    },
+    decrement(){
+      this.count--;
+
+    }
+  }
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>{{ name }}</h1>
+  <h2>Counter: {{count  }} </h2>
+  <button @click="increment">+</button>
+  <button @click="decrement">-</button>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+button {
+  margin: 5px;
+  padding: 12px;
+  border: none;
+  cursor: pointer;
 }
 </style>
+
+
