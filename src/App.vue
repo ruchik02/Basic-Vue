dd003bcbf5d5d6ceb63f5cd9badac15c95001837
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-02-25 15:09:38 +0530

    example class and bindings

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 // import Example15 from './components/Example15.vue';
-import Example16 from './components/Example16.vue';
+// import Example16 from './components/Example16.vue';
+import Example17 from './components/Example17.vue';
 export default {
   components: {
     // Example15,
-    Example16
+    // Example16,
+    Example17
   }
 }
 </script>
 <template>
   <!-- <Example15 /> -->
-   <Example16 />
+   <!-- <Example16 /> -->
+    <Example17 />
 </template>
---
+++ src/components/Example17.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      isActive: false,
+      isSuccess: true,
+      isLarge: true,
+    };
+  },
+  computed: {
+    activeClass() {
+      return this.isActive ? "bg-blue" : "bg-red";
+    },
+  },
+  methods: {
+    toggleClass() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+<template>
+  <v-container>
+    <v-btn :class="activeClass" @click="toggleClass">Toggle Button</v-btn>
+    <v-btn
+      :class="{ 'text-white': true }"
+      :color="isSuccess ? 'success' : 'red'"
+      >Success Button</v-btn
+    >
+    <v-btn :class="['bg-primary', isLarge ? 'text-h5' : 'text-body-2']">
+      Dynamic Button
+    </v-btn>
+  </v-container>
+</template>
+<style scoped>
+.bg-blue {
+  background-color: blue !important;
+  color: white;
+}
+.bg-red {
+  background-color: red !important;
+  color: white;
+}
+</style>
